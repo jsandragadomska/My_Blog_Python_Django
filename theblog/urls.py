e54ee3fb48dbdb5d429d6bbdec08052b43840e7c
jsandragadomska <gadomska.sandra@gmail.com>
2020-08-28 13:05:22 +0200

    New Category button for user page

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import HomeView, PostView, AddPostView, UpdatePostView, DeletePostView
+from .views import HomeView, PostView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('/post/<int:pk>', PostView.as_view(), name='post_details'),
     path('post/add', AddPostView.as_view(), name='add_post'),
     path('post/edit/<int:pk>', UpdatePostView.as_view(), name='edit_post'),
-    path('post/delete/<int:pk>', DeletePostView.as_view(), name='delete_post')
+    path('post/delete/<int:pk>', DeletePostView.as_view(), name='delete_post'),
+    path('category/add', AddCategoryView.as_view(), name='add_category'),
 ]
