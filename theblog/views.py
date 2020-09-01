26a291c11fe19869605c97232e6d8a2d73b849ce
jsandragadomska <gadomska.sandra@gmail.com>
2020-09-01 11:34:38 +0200

    Categories urls updated

--- theblog/views.py
+++ theblog/views.py
@@ -7,8 +7,8 @@ from django.urls import reverse_lazy
 #def home(request):
 #	return render(request, 'home.html', {})
 def CategoryView(request, cats):
-	category_posts = Post.objects.filter(category=cats)
-	return render(request, 'categories.html', {'cats':cats.title(), 'category_posts':category_posts})
+	category_posts = Post.objects.filter(category=cats.replace('-', ' '))
+	return render(request, 'categories.html', {'cats':cats.title().replace('-', ' '), 'category_posts':category_posts})
 
 class HomeView(ListView):
 	model = Post
