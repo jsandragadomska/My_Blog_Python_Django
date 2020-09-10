4b62acddcaa915429b2bc95b39143ccaa0cbed29
jsandragadomska <gadomska.sandra@gmail.com>
2020-09-10 13:31:21 +0200

    Unlike post button, handling likes for unlogged users

--- theblog/views.py
+++ theblog/views.py
@@ -5,69 +5,90 @@ from .models import Post, Category
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy, reverse
 
-#def home(request):
+# def home(request):
 #	return render(request, 'home.html', {})
-class HomeView(ListView):
-	model = Post
-	template_name = 'home.html'
-	ordering = ['-post_date']
 
-	def get_context_data(self, *args, **kwargs):
-		cat_menu = Category.objects.all()
-		context = super(HomeView, self).get_context_data(*args, **kwargs)
-		context['cat_menu'] = cat_menu
-		return context
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+    ordering = ['-post_date']
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context['cat_menu'] = cat_menu
+        return context
+
 
 def CategoryView(request, cats):
-	category_posts = Post.objects.filter(category=cats.replace('-', ' '))
-	return render(request, 'categories.html', {'cats':cats.title().replace('-', ' '), 'category_posts':category_posts})
+    category_posts = Post.objects.filter(category=cats.replace('-', ' '))
+    return render(request, 'categories.html', {'cats': cats.title().replace('-', ' '), 'category_posts': category_posts})
+
 
 def CategoryListView(request):
-	cat_menu_list = Category.objects.all()
-	return render(request, 'category_list.html', {'cat_menu_list':cat_menu_list})
+    cat_menu_list = Category.objects.all()
+    return render(request, 'category_list.html', {'cat_menu_list': cat_menu_list})
+
 
 def LikeView(request, pk):
-	post = get_object_or_404(Post, id=request.POST.get('post_id'))
-	post.likes.add(request.user)
-	return HttpResponseRedirect(reverse('post_details', args=[str(pk)]))
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+
+    return HttpResponseRedirect(reverse('post_details', args=[str(pk)]))
+
 
 class PostView(DetailView):
-	model = Post
-	template_name = 'post_details.html'
+    model = Post
+    template_name = 'post_details.html'
 
-	def get_context_data(self, *args, **kwargs):
-		cat_menu = Category.objects.all()
-		stuff = get_object_or_404(Post, id=self.kwargs['pk'])
-		total_likes = stuff.total_likes()
-		context = super(PostView, self).get_context_data(*args, **kwargs)
-		context['cat_menu'] = cat_menu
-		context['total_likes'] = total_likes
-		return context
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        stuff = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = stuff.total_likes()
+
+        liked = False
+        if stuff.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        context = super(PostView, self).get_context_data(*args, **kwargs)
+        context['cat_menu'] = cat_menu
+        context['total_likes'] = total_likes
+        context['liked'] = liked
+        return context
 
 
 class AddPostView(CreateView):
-	model = Post
-	form_class = PostForm
-	template_name = 'add_post.html'
-	#fields = '__all__'
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+    #fields = '__all__'
+
 
 class AddCategoryView(CreateView):
-	model = Category
-	template_name = 'add_category.html'
-	fields = '__all__'
+    model = Category
+    template_name = 'add_category.html'
+    fields = '__all__'
 
 # To do : create form to update category name
-#class UpdateCategoryView(UpdateView):
+# class UpdateCategoryView(UpdateView):
 #	model = Category
 
+
 class UpdatePostView(UpdateView):
-	model = Post
-	form_class = EditForm
-	template_name = 'update_post.html'
-	#fields = ('title', 'body')
+    model = Post
+    form_class = EditForm
+    template_name = 'update_post.html'
+    #fields = ('title', 'body')
+
 
 class DeletePostView(DeleteView):
-	model = Post
-	template_name = 'delete_post.html'
-	success_url = reverse_lazy('home')
-
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
