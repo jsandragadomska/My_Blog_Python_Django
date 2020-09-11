dbc64c873a6c38ebf6281b034e7da600aabf9ab2
jsandragadomska <gadomska.sandra@gmail.com>
2020-09-11 11:46:17 +0200

    Rich TextField imported for blog post body

--- theblog/models.py
+++ theblog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
+from ckeditor.fields import RichTextField
 
 class Category(models.Model):
     name = models.CharField(max_length=225, default='None')
@@ -16,7 +17,8 @@ class Post(models.Model):
     title = models.CharField(max_length=225)
     title_tag = models.CharField(max_length=225, default="Blogpost")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    body = RichTextField(blank=True, null=True)
+    #body = models.TextField()
     post_date = models.DateField(auto_now_add=True, blank=True)
     category = models.CharField(max_length=225, default='None')
     likes = models.ManyToManyField(User, related_name='blog_posts')
