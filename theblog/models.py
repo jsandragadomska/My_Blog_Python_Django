a1afd134f12302dff28b18d5794e054af3784c30
jsandragadomska <gadomska.sandra@gmail.com>
2020-09-25 13:51:28 +0200

    New model has added - comment

--- theblog/models.py
+++ theblog/models.py
@@ -48,3 +48,12 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("home")
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=225)
+    body = models.TextField()
+    comment_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
